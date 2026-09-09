d1d1bde3d53513482d209e099caed2701147f9d2
pocketmelody-momodaisuki <pocketmelody@gmail.com>
2026-09-09 16:08:17 +0900

    change

--- game.js
+++ game.js
@@ -24,8 +24,8 @@ canvas.height = 600;
 
 function applyDisplaySize() {
     if (isiPhone) {
-        canvas.style.width = "100vw";
-        canvas.style.height = "30vw";
+        canvas.style.width = "60vw";
+        canvas.style.height = "50vw";
     } else {
         canvas.style.width = "1200px";
         canvas.style.height = "600px";
